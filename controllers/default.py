4591c24da4b06ad37a22a5d87ccee7c4f2d936fd
Zaxim <zdehlawi@gmail.com>
2014-01-10 09:30:09 -0800

    change some files trying to get bootstrap to work

--- controllers/default.py
+++ controllers/default.py
@@ -74,3 +74,10 @@ def data():
       LOAD('default','data.load',args='tables',ajax=True,user_signature=True)
     """
     return dict(form=crud())
+
+def legal():
+    return dict()
+
+def about():
+    return dict()
+    
