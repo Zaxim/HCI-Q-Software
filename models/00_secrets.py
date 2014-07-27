9759763c0ff7e361b07e75b69112da6480a90a0c
Zaxim <zdehlawi@gmail.com>
2014-07-27 14:25:45 -0700

    Added default config file

---
+++ models/00_secrets.py
@@ -0,0 +1,7 @@
+# Use this file to setup SMTP support for qollate.
+configz = {
+    'server': 'smtp.gmail.com:587',
+    'username': '<USERNAME>@gmail.com',
+
+    'login': '<USERNAME>:<PASSWORD>'
+}
